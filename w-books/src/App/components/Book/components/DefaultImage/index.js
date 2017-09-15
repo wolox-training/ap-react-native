79d87fb9ae02d9f65da860f3374ebe285b656e02
adrian-ponce-wolox <adrian.ponce@wolox.com.ar>
2017-09-15 12:43:54 -0300

    Remove unnecessary size of DefaultImage

--- w-books/src/App/components/Book/components/DefaultImage/index.js
+++ w-books/src/App/components/Book/components/DefaultImage/index.js
@@ -5,7 +5,7 @@ import './styles.css';
 function DefaultImage() {
   return (
     <div className="DefaultImage">
-      <img src={defaultimage} className="DefaultImage-logo" width="75" height="75" alt="DefaultImage Logo" />
+      <img src={defaultimage} className="DefaultImage-logo" alt="DefaultImage Logo" />
     </div>
    )
  }
