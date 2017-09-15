1feb5504f7b4294f4a762c96ccb4ef376d67447b
adrian-ponce-wolox <adrian.ponce@wolox.com.ar>
2017-09-15 14:09:09 -0300

    Use ECMA to deconstruct function on Book

--- w-books/src/App/components/Book/index.js
+++ w-books/src/App/components/Book/index.js
@@ -3,15 +3,12 @@ import './styles.css';
 import DefaultImage from './components/DefaultImage/index.js'
 
 function Book(props) {
-  let image = null;
-  if (props.imageUrl !== null) {
-    image = <img src={props.imageUrl} className="Book-image" alt="Portada" />;
-  } else {
-    image = <DefaultImage />;
-  }
   return (
     <div className="Book">
-      {image}
+      { props.imageUrl !== null ?
+        <img src={props.imageUrl} className="Book-image" alt="Portada" /> :
+        <DefaultImage />
+      }
       <h2 className="Book-title">{props.title}</h2>
       <h2 className="Book-author">{props.author}</h2>
     </div>
