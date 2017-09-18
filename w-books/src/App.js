82df7bd8cfcbd1ac578a6ec608aecb960cfba0d9
adrian-ponce-wolox <adrian.ponce@wolox.com.ar>
2017-09-18 15:42:19 -0300

    Implements 'dashboard' and 'book/:bookId' routes

--- w-books/src/App.js
+++ w-books/src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import Header from './App/components/Header/index.js';
 import Home from './App/screens/Home/index.js'
+import BookDetail from './App/screens/BookDetail';
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Home />
-      </div>
+      <Router>
+        <div className="App">
+            <Header />
+            <Route path="/dashboard" component={Home}/>
+            <Route path="/book/:bookId" component={BookDetail}/>
+        </div>
+      </Router>
     );
   }
 }
--- w-books/src/App/components/Book/index.js
+++ w-books/src/App/components/Book/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import './styles.css';
 import DefaultImage from './components/DefaultImage/index.js'
 
 function Book(props) {
   return (
-    <div className="Book">
-      { props.imageUrl !== null ?
-        <img src={props.imageUrl} className="Book-image" alt="Portada" /> :
-        <DefaultImage />
-      }
-      <h2 className="Book-title">{props.title}</h2>
-      <h2 className="Book-author">{props.author}</h2>
-    </div>
+    <Link to={`book/${props.book.id}`}>
+      <div className="Book" >
+        { props.book.image_url !== null ?
+          <img src={props.book.image_url} className="Book-image" alt="Portada" /> :
+          <DefaultImage />
+        }
+        <h2 className="Book-title">{props.book.title}</h2>
+        <h2 className="Book-author">{props.book.author}</h2>
+      </div>
+    </Link>
   );
 }
 
