85ceec9334a931b7cf22d20565797d4bd27fdc30
adrian-ponce-wolox <adrian.ponce@wolox.com.ar>
2017-09-19 16:50:59 -0300

    Remove unnecessary import of react-router-dom

--- w-books/src/App.js
+++ w-books/src/App.js
@@ -1,9 +1,4 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
 import Header from './App/components/Header/index.js';
 import AppRoutes from './config/routes.js';
 
