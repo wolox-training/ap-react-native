5161960e857c6b820f27c261e1b2e174b0fd85a8
adrian-ponce-wolox <adrian.ponce@wolox.com.ar>
2017-09-20 18:28:44 -0300

    Fix warning: Use numbers instead of strings for Cover

--- w-books/src/App/components/Book/index.js
+++ w-books/src/App/components/Book/index.js
@@ -7,7 +7,7 @@ function Book(props) {
   return (
     <Link to={`book/${props.book.id}`}>
       <div className="Book" >
-        <Cover height="182" width="128" imageUrl={props.book.image_url}/>
+        <Cover height={182} width={128} imageUrl={props.book.image_url}/>
         <h2 className="Book-title">{props.book.title}</h2>
         <h2 className="Book-author">{props.book.author}</h2>
       </div>
