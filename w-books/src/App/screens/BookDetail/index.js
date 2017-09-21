b21439fdbe4f2f9ae9268220e86e20efcb16598e
adrian-ponce-wolox <adrian.ponce@wolox.com.ar>
2017-09-21 16:43:35 -0300

    Moved bookFind to componentWillMount() on BookDetail

--- w-books/src/App/screens/BookDetail/index.js
+++ w-books/src/App/screens/BookDetail/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import './styles.css';
 import BookList from '../../../assets/dummy-books.json';
 import CommentList from '../../../assets/dummy-comments.json';
 import BookDetail from './layout.js'
 
-function BookDetailContainer({ match }) {
-  console.log("BookList: " + BookList)
-  console.log("match.params.bookId" + match.params.bookId)
-  let book = BookList.find((element) => { return element.id == match.params.bookId})
-  return <BookDetail book={book}
-                     suggestions={getSuggestions()}
-                     comments={CommentList} />
+class BookDetailContainer extends Component {
+  constructor({match}) {
+    super()
+    this.state = { bookId: match.params.bookId, book: null }
+  }
+  componentWillMount() {
+    let book = BookList.find((element) => { return element.id == this.state.bookId})
+    this.setState({book: book})
+  }
+  render() {
+    return <BookDetail book={this.state.book}
+                       suggestions={getSuggestions()}
+                       comments={CommentList} />
+  }
 }
 
 function getSuggestions() {
