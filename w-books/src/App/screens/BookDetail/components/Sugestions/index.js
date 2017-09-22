62102872f285b0491f9a0142210fb8842772af7d
adrian-ponce-wolox <adrian.ponce@wolox.com.ar>
2017-09-22 10:55:10 -0300

    Add strings.js file to Suggestions

--- w-books/src/App/screens/BookDetail/components/Sugestions/index.js
+++ w-books/src/App/screens/BookDetail/components/Sugestions/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './styles.css';
 import Cover from '../../../../components/Cover/index.js'
+import {SUGGESTIONS} from './strings.js'
 
 function Sugestions({books}) {
   return (
     <div className="Sugestions">
-      <h2>Sugerencias</h2>
+      <h2>{SUGGESTIONS}</h2>
       <div className="Sugestions-booklist">
         {books.map(book =>
           <div className="Sugestions-cover">
