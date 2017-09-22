a106049e6ba4997117ca9efa7e8f5c50c922c51e
adrian-ponce-wolox <adrian.ponce@wolox.com.ar>
2017-09-22 10:31:23 -0300

    Fix warnings on height/width type use on Cover

--- w-books/src/App/screens/BookDetail/components/Sugestions/index.js
+++ w-books/src/App/screens/BookDetail/components/Sugestions/index.js
@@ -9,7 +9,7 @@ function Sugestions({books}) {
       <div className="Sugestions-booklist">
         {books.map(book =>
           <div className="Sugestions-cover">
-            <Cover imageUrl={book.image_url} height="124" width="87" />
+            <Cover imageUrl={book.image_url} height={124} width={87} />
           </div>
         )}
       </div>
