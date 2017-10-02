562ac165d54dcd8b0b6f18d6a04faa64070695fb
adrian-ponce-wolox <adrian.ponce@wolox.com.ar>
2017-10-02 15:40:09 -0300

    Add style to contacts

--- wchat/screens/ContactList/components/ContactCell/index.js
+++ wchat/screens/ContactList/components/ContactCell/index.js
@@ -5,8 +5,14 @@ export default class ContactCell extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Image style={styles.avatar} source={{uri: this.props.avatar}} />
-        <Text style={styles.name}>{this.props.username} </Text>
+        <View style={styles.content}>
+          <Image style={styles.avatar} source={{uri: this.props.avatar}} />
+          <View style={styles.info}>
+            <Text style={styles.name}>{this.props.username} </Text>
+            <Text style={styles.message}>Lorem ipsum dolor sit amet...</Text>
+          </View>
+        </View>
+        <View style={styles.separator} />
       </View>
     );
   }
@@ -15,17 +21,39 @@ export default class ContactCell extends Component {
 const styles = StyleSheet.create({
   container: {
    flex: 1,
-   flexDirection: 'row',
-   paddingTop: 22
   },
-  name: {
-    fontSize: 20,
-    textAlign: 'left',
-    margin: 10,
+  content: {
+   flex: 1,
+   flexDirection: 'row',
+   marginVertical: 12,
   },
   avatar: {
     height: 44,
     width: 44,
     marginLeft: 16,
+    borderRadius: 22,
+    overflow: 'hidden',
   },
+  info: {
+   flex: 1,
+   marginLeft: 10,
+   justifyContent: 'center',
+  },
+  name: {
+    fontSize: 16,
+    textAlign: 'left',
+    fontWeight: 'bold',
+    marginBottom: 4,
+  },
+  message: {
+    fontSize: 14,
+    textAlign: 'left',
+    color: '#999',
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#CCC',
+    marginRight: 10,
+    marginLeft: 70,
+  }
 })
