a125f3c3b56cb646a54aa87e28ee4883e37b192c
adrian-ponce-wolox <adrian.ponce@wolox.com.ar>
2017-10-05 16:59:15 -0300

    Messages styles acoordingly

--- wchat/screens/shared/components/MessageCell/index.js
+++ wchat/screens/shared/components/MessageCell/index.js
@@ -4,7 +4,7 @@ import styles from './styles.js'
 
 export default function MessageCell(props) {
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, props.isOwner? styles.owner : styles.contact]}>
       <Text style={styles.text}>{props.text}</Text>
       <Text style={styles.time}>{props.time}</Text>
     </View>
--- wchat/screens/shared/components/MessageCell/styles.js
+++ wchat/screens/shared/components/MessageCell/styles.js
@@ -1,15 +1,21 @@
 import { AppRegistry, StyleSheet, Text, Image, View } from 'react-native';
 import { mainColor, secondaryColor } from '../../../../shared/utils/colors.js'
 
-const avatarSize = 44;
-
 const styles = StyleSheet.create({
   container: {
    padding: 12,
-   borderRadius: 16,
-   backgroundColor: secondaryColor,
    marginBottom: 10,
-   width: '85%'
+   width: '85%',
+   borderRadius: 16
+  },
+  owner: {
+   borderTopRightRadius: 4,
+   backgroundColor: mainColor,
+   alignSelf: 'flex-end'
+  },
+  contact: {
+   borderTopLeftRadius: 4,
+   backgroundColor: secondaryColor
   },
   text: {
     fontSize: 18,
